c058ef5d3c6c99d0d568b1df0f27c4c7fe5843be
Razvan Tomegea <razvan.tomegea@edetek.com>
2022-08-02 17:26:07 +0300

    Added first page: NFT Ticket verification QR code scanning based on Maiar Wallet

--- src/pages/UnlockPage/index.tsx
+++ src/pages/UnlockPage/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { useGetLoginInfo } from '@elrondnetwork/dapp-core/hooks';
-import {
-  ExtensionLoginButton,
-  WebWalletLoginButton,
-  LedgerLoginButton,
-  WalletConnectLoginButton
-} from '@elrondnetwork/dapp-core/UI';
+import { WalletConnectLoginButton } from '@elrondnetwork/dapp-core/UI';
 import { routeNames } from 'routes';
 
 export const UnlockRoute: () => JSX.Element = () => {
@@ -18,34 +13,15 @@ export const UnlockRoute: () => JSX.Element = () => {
   }, [isLoggedIn]);
 
   return (
-    <div className='home d-flex flex-fill align-items-center'>
-      <div className='m-auto' data-testid='unlockPage'>
-        <div className='card my-4 text-center'>
-          <div className='card-body py-4 px-2 px-sm-2 mx-lg-4'>
-            <h4 className='mb-4'>Login</h4>
-            <p className='mb-4'>pick a login method</p>
-
-            <ExtensionLoginButton
-              callbackRoute={routeNames.dashboard}
-              loginButtonText={'Extension'}
-            />
-            <WebWalletLoginButton
-              callbackRoute={routeNames.dashboard}
-              loginButtonText={'Web wallet'}
-            />
-            <LedgerLoginButton
-              loginButtonText={'Ledger'}
-              callbackRoute={routeNames.dashboard}
-              className={'test-class_name'}
-            />
-            <WalletConnectLoginButton
-              callbackRoute={routeNames.dashboard}
-              loginButtonText={'Maiar'}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <WalletConnectLoginButton
+      callbackRoute={routeNames.dashboard}
+      className='button--hidden'
+      lead='Two transactions will be required'
+      loginButtonText={''}
+      shouldRenderDefaultCss={false}
+      title='Scan the QR using Maiar App'
+      wrapContentInsideModal={false}
+    />
   );
 };
 
