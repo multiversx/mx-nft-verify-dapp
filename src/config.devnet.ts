f8251016136afcddd45a4947376cd225215adc0d
Theodor Dumitrache <theodor.dumitrache@yahoo.com>
2023-06-07 14:59:27 +0300

    added config builds

---
+++ src/config.devnet.ts
@@ -0,0 +1,15 @@
+import { EnvironmentsEnum } from '@multiversx/sdk-dapp/types';
+
+export const collection = ''; // TODO: This must be set
+export const pixel = ''; // TODO: This must be set
+export const callback = ''; // TODO: This must be set
+export const age = ''; // TODO: This must be set
+export const ref = ''; // TODO: This must be set
+
+export const dappEnvironment = EnvironmentsEnum.devnet;
+
+export const dAppName = 'NFT Ownership Verifier';
+
+export const walletConnectV2ProjectId = 'd5cba8101a9a190ef16230c4c03554cf';
+
+export const apiTimeout = 6000;
---
+++ src/config.mainnet.ts
@@ -0,0 +1,15 @@
+import { EnvironmentsEnum } from '@multiversx/sdk-dapp/types';
+
+export const collection = ''; // TODO: This must be set
+export const pixel = ''; // TODO: This must be set
+export const callback = ''; // TODO: This must be set
+export const age = ''; // TODO: This must be set
+export const ref = ''; // TODO: This must be set
+
+export const dAppName = 'NFT Ownership Verifier';
+
+export const dappEnvironment = EnvironmentsEnum.mainnet;
+
+export const walletConnectV2ProjectId = '138fd3454a31d70561484e0b7b88eab0';
+
+export const apiTimeout = 6000;
